71f473b523322a4bbb6344a1c00fbc66f7d1a009
Cody Fontes <codemensch@gmail.com>
2018-07-10 21:10:20 -0700

    Added all soundcloud iframes, edits to main js.

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@ $(document).ready(function(){
   (function(){
     var widgetIframe = document.getElementById('sc-widget'),
         widget       = SC.Widget(widgetIframe);
+    var vintageIframe = document.getElementById('vintage-widget'),
+        vintage       = SC.Widget(vintageIframe);
+    var rocketIframe = document.getElementById('rocket-widget'),
+        rocket       = SC.Widget(rocketIframe);    
 
-    widget.bind(SC.Widget.Events.READY, function() {
-      widget.play();
+    widget.bind(SC.Widget.Events.READY, function() {      
       
-      // get current level of volume
-      widget.getVolume(function(volume) {
-        console.log('current volume value is ' + volume);
-      });
-      // set new volume level
-      widget.setVolume(50);
-      // get the value of the current position
+    });
+
+    vintage.bind(SC.Widget.Events.READY, function() {
+     
+    });
+
+    rocket.bind(SC.Widget.Events.READY, function() {
+     
     });
 
   }());
