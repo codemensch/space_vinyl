67d95d90a67ebc74fe2104d673d0a62a56850c32
Cody Fontes <codemensch@gmail.com>
2018-07-19 21:06:22 -0700

    Completed functioning album stack

--- js/main.js
+++ js/main.js
@@ -1,6 +1,44 @@
 $( document ).ready(function() {
-  
-
+  //Choose Album/Playlist
+  $('i:nth-child(1), i:nth-child(2)').click(function(){
+    //Put all albums in a node
+    let albums = document.getElementsByClassName('album');
+    //Click previous button
+    if (this.id == 'prev'){
+      //Pull out album from bottom of stack
+      $(albums[0]).addClass('animate-album');
+      //Wait for animation to finish
+      $(albums[0]).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+        //Set explicit element margin
+        $(albums[0]).css('margin-left', '400px');
+        //Remove animation styles/margin so doesn't re-fire on append
+        $(albums[0]).removeClass('animate-album');
+        //Put bottom album on top
+        $('#stack').append($(albums[0]));
+        //Force browser to calculate margin value
+        $(albums[albums.length - 1]).css('margin-left');
+        //Transition album back to original position
+        $(albums[albums.length - 1]).css('margin-left', '0px');
+      });
+    }
+    else{
+      //Push album to bottom of stack
+      $(albums[albums.length - 1]).addClass('animate-album');
+      //Wait for animation to finish
+      $(albums[albums.length - 1]).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+        //Set explicit element margin
+        $(albums[albums.length - 1]).css('margin-left', '400px');
+        //Remove animation styles/margin so doesn't re-fire on prepend
+        $(albums[albums.length - 1]).removeClass('animate-album');
+        //Put top album on bottom
+        $('#stack').prepend($(albums[albums.length - 1]));
+        //Force browser to calculate margin value
+        $(albums[0]).css('margin-left');
+        //Transition album back to original position
+        $(albums[0]).css('margin-left', '0px');
+      });
+    }
+  });
   /*
   $('#sc-widget').on('load', function(){
 
